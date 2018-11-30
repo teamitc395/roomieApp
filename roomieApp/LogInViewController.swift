af9c68b63ed67790109b23860420a61a67eb9848
Kenia Rioja <kdrioja@gmail.com>
2018-11-30 15:22:58 -0800

    Added LogInVC and SignUpVC

---
+++ roomieApp/LogInViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LogInViewController.swift
+//  roomieApp
+//
+//  Created by user145766 on 11/30/18.
+//  Copyright © 2018 Kenia Rioja. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
