4721721e571b1427df3ac0456cb79ead8e97803a
Kenia Rioja <kdrioja@gmail.com>
2018-11-30 16:35:37 -0800

    Connected Log In and Sign Up pages via segues

--- roomieApp/SignUpViewController.swift
+++ roomieApp/SignUpViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var groupIDTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onTapSignUp(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func onTapLogIn(_ sender: Any) {
+        performSegue(withIdentifier: "logInSegue", sender: nil)
+    }
+    
 }
